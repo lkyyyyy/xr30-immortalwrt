80745ca3675dc1e95d876f95cea9d0d818e9b655
lkyyyyy <86304293+lkyyyyy@users.noreply.github.com>
2026-08-29 17:51:36 +0800

    Add files/etc/uci-defaults/90-enable-hnat.sh

---
+++ files/etc/uci-defaults/90-enable-hnat.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 开启 MTK 硬件 NAT：flow offload + hardware offload
+uci set firewall.@defaults[0].flow_offloading='1'
+uci set firewall.@defaults[0].flow_offloading_hw='1'
+uci commit firewall
+
+exit 0
+
